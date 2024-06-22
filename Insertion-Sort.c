628ae2888a712b86f5a812a25cd75b8325064782
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-06-22 16:45:45 +0500

    Added insertion sort in c

---
+++ Insertion-Sort.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void InsertionSort(int array[], int n){
+    int i, j, key;
+    for(i = 0; i < n; i++){
+        key = array[i];
+        j = i - 1;
+        while(j >= 0 && array[j] > key){
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = key;
+    }
+}
+
+int main(){
+    int array[] = {4,2,3,1};
+    int n = sizeof(array) / sizeof(array[0]);
+    InsertionSort(array, n);
+    for(int i = 0; i < n; i++){
+        printf("%d, ", array[i]);
+    }
+}
